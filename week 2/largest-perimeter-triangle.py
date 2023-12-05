6e597e09384138efa6193e8150c017a627681232
DanielKinnito <100097646+DanielKinnito@users.noreply.github.com>
2023-12-05 19:56:10 +0300

    Added solution for Largest Perimeter Triangle

---
+++ week 2/largest-perimeter-triangle.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def largestPerimeter(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort(reverse=True)  
+        length = len(nums)
+        for i in range(length - 2):  
+            if nums[i] < nums[i + 1] + nums[i + 2]:
+                return nums[i] + nums[i + 1] + nums[i + 2]
+        return 0
