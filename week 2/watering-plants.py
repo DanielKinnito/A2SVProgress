44981cd24cb3c614b41c7bd985dd749e8cad9f89
DanielKinnito <100097646+DanielKinnito@users.noreply.github.com>
2023-12-05 21:35:13 +0300

    Added solution for Watering Plants

---
+++ week 2/watering-plants.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def wateringPlants(self, plants, capacity):
+        """
+        :type plants: List[int]
+        :type capacity: int
+        :rtype: int
+        """
+        steps = -1
+        remaining_water = capacity
+
+        for i in range(len(plants)):
+            if remaining_water >= plants[i]:
+                remaining_water -= plants[i]
+                steps += 1
+            else:
+                steps += (i * 2) + 1
+                remaining_water = capacity
+                remaining_water -= plants[i]
+
+        return steps + 1
