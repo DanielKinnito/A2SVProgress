e4ecd417631ac499bd7b9846653eeabc3756b8bf
DanielKinnito <100097646+DanielKinnito@users.noreply.github.com>
2023-12-12 18:48:34 +0300

    Added solution for Replace Elements in an Array

---
+++ week 3/replace-elements-in-an-array.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def arrayChange(self, nums: List[int], operations: List[List[int]]) -> List[int]:
+        """
+        1- Store the value and index in nums on a dictionary
+        2- for the operations in operations, compare the first number 
+            with the dictionary and replace that number with the second
+            - and delete the first instance from the dict{}
+        """
+        nums_dict = {value: index for index, value in enumerate(nums)}
+        
+        for operation in operations:
+            if operation[0] in nums_dict:
+                index_to_update = nums_dict[operation[0]]
+                
+                nums[index_to_update] = operation[1]
+                nums_dict[operation[1]] = index_to_update
+                
+                del nums_dict[operation[0]]
+        
+        return nums
