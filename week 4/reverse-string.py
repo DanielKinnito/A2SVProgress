864cf7684669a17aa5fd04140f2829ec067de406
DanielKinnito <100097646+DanielKinnito@users.noreply.github.com>
2023-12-19 19:59:34 +0300

    Added solution for Reverse String

---
+++ week 4/reverse-string.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        n = len(s)
+        front = 0
+        back = n-1
+        while front < n/2 :
+            s[front], s[back] = s[back], s[front]
+
+            front += 1
+            back -= 1
