8213b28ab7be492352b4cbff7df11dda06db96a4
DanielKinnito <100097646+DanielKinnito@users.noreply.github.com>
2023-12-22 17:43:22 +0300

    Added solution for Reverse String

--- week 4/reverse-string.py
+++ week 4/reverse-string.py
@@ -4,11 +4,6 @@ class Solution:
         Do not return anything, modify s in-place instead.
         """
         n = len(s)
-        front = 0
-        back = n-1
         
-        while front < n/2 :
-            s[front], s[back] = s[back], s[front]
-
-            front += 1
-            back -= 1
+        for i in range(n//2):
+            s[i], s[n -1 -i] = s[n -1 -i], s[i] 
