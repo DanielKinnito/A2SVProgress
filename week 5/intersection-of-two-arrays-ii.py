87acdf6a11ec886fff520b713ab42d54e70001f0
DanielKinnito <100097646+DanielKinnito@users.noreply.github.com>
2023-12-28 16:26:25 +0300

    Added solution for Intersection of Two Arrays II

--- week 5/intersection-of-two-arrays-ii.py
+++ week 5/intersection-of-two-arrays-ii.py
@@ -4,15 +4,17 @@ class Solution:
         nums2.sort()
         
         answer = []
-
         i, j = 0, 0
+
         while i < len(nums1) and j < len(nums2):
             if nums1[i] == nums2[j]:
                 answer.append(nums1[i])
-                i += 1
-                j += 1
+                nums1.pop(i)
+                nums2.pop(j)
+            
             elif nums1[i] < nums2[j]:
                 i += 1
+            
             else:
                 j += 1
 
