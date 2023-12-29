363c78b8abad0f7929f1727c46bea0910fb00ecc
DanielKinnito <100097646+DanielKinnito@users.noreply.github.com>
2023-12-29 14:58:26 +0300

    Added solution for Sorting the Sentence

--- week 5/sorting-the-sentence.py
+++ week 5/sorting-the-sentence.py
@@ -1,5 +1,11 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
+        """
+        1- cast the string to a list
+        2- make an answer array of the same length
+        3- for every word take its last character(usually a number) and store the rest in the answer array at position described by the last char -1.
+        4- join the elements of the list with a space and return
+        """
         words = s.split()
         n = len(words)
         answer = [""] * n
