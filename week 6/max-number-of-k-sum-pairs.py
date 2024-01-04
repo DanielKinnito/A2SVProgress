b9af950444946f42615419baa0903d8047c35dfa
DanielKinnito <100097646+DanielKinnito@users.noreply.github.com>
2024-01-04 18:42:55 +0300

    Added solution for Max Number of K-Sum Pairs

---
+++ week 6/max-number-of-k-sum-pairs.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+
+        left = 0
+        right = len(nums) - 1
+
+        answer = 0
+        while left < right:
+            if nums[left] + nums[right] == k:
+                answer += 1
+                left += 1
+                right -= 1
+
+            elif nums[left] + nums[right] < k:
+                left += 1
+            
+            else:
+                right -= 1
+        
+        return answer
