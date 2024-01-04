a71282140f1f311b65b648d9baf64ab337f46520
DanielKinnito <100097646+DanielKinnito@users.noreply.github.com>
2024-01-04 17:27:53 +0300

    Added solution for Minimum Processing Time

--- week 6/minimum-processing-time.py
+++ week 6/minimum-processing-time.py
@@ -6,6 +6,7 @@ class Solution:
         max_time = 0
 
         for i in range(len(tasks)):
+            # calculate in intervals of 4
             max_time = max(max_time, processorTime[i // 4] + tasks[i])
 
         return max_time
