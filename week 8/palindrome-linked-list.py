b5deaa77feed39db8eb9b25a3733373e81c39d7e
DanielKinnito <100097646+DanielKinnito@users.noreply.github.com>
2024-01-24 08:32:45 -0800

    Added solution for Palindrome Linked List

--- week 8/palindrome-linked-list.py
+++ week 8/palindrome-linked-list.py
@@ -5,11 +5,31 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        curr = head
-        array = []
+        # Find the middle
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Reverse the second half
+        prev = None
+        curr = slow
+        
 
         while curr:
-            array.append(curr.val)
-            curr = curr.next
-        
-        return array == array[::-1]
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+
+        # Compare the two halves
+        ptr_one = head
+        ptr_two = prev
+
+        while ptr_two:
+            if ptr_one.val != ptr_two.val:
+                return False
+            ptr_two = ptr_two.next
+            ptr_one = ptr_one.next
+
+        return True
