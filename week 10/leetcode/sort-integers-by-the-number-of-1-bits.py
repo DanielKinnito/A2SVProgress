70a98a1646080deedd6343fa281284a7accacbd9
DanielKinnito <100097646+DanielKinnito@users.noreply.github.com>
2024-02-06 12:26:01 -0800

    Add solution for Sort Integers by The Number of 1 Bits

--- week 10/leetcode/sort-integers-by-the-number-of-1-bits.py
+++ week 10/leetcode/sort-integers-by-the-number-of-1-bits.py
@@ -1,11 +1,4 @@
 class Solution:
     def sortByBits(self, arr: List[int]) -> List[int]:
-        def count_ones(num):
-            count = 0
-            while num:
-                count += num & 1
-                num >>= 1
-            return count
-            
-        arr.sort(key = lambda x: (count_ones(x), x))
+        arr.sort(key=lambda x: (bin(x).count('1'), x))
         return arr
