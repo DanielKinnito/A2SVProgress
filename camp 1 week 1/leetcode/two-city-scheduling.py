a673338286e2c2e8f15dd0bb1ce27fa53126d373
DanielKinnito <100097646+DanielKinnito@users.noreply.github.com>
2024-02-21 07:35:20 -0800

    Add solution for Two City Scheduling

--- camp 1 week 1/leetcode/two-city-scheduling.py
+++ camp 1 week 1/leetcode/two-city-scheduling.py
@@ -1,30 +1,9 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        """ Sort based on difference and reverse it.
-            add for the counts of a and b until they are n/2
-           """
-        answer = 0
         n = len(costs)
+        half = n//2
 
-        costs.sort(key=lambda cost: abs(cost[0] - cost[1]), reverse=True)
+        total = [(a, b, a-b) for a, b in costs]
+        total.sort(key = lambda x: x[2])
 
-        countA = 0
-        countB = 0
-
-        for cost in costs:
-            if cost[0] < cost[1]:
-                if countA < n // 2:
-                    answer += cost[0]
-                    countA += 1
-                else:
-                    answer += cost[1]
-                    countB += 1
-            else:
-                if countB < n // 2:
-                    answer += cost[1]
-                    countB += 1
-                else:
-                    answer += cost[0]
-                    countA += 1
-
-        return answer
+        return sum(a for a, b, difference in total[:half]) + sum(b for a, b, difference in total[half:])
