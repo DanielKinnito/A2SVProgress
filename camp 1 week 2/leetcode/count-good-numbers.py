4bcea20aa2eadff45f84226bb78da00c95d04dad
DanielKinnito <100097646+DanielKinnito@users.noreply.github.com>
2024-02-26 08:05:04 -0800

    Add solution for Count Good Numbers

--- camp 1 week 2/leetcode/count-good-numbers.py
+++ camp 1 week 2/leetcode/count-good-numbers.py
@@ -7,15 +7,7 @@ class Solution(object):
         MOD = 10**9+7
         even_counts=n/2 if n%2 ==0 else n//2+1
         odd_counts=n//2
-        # def pow(x,n):
-        #     if n==0:
-        #         return 1
-        #     if n==1:
-        #         return x
-        #     half =pow(x,n//2)
-        #     if n%2==0:
-        #         return (half*half)%MOD
-        #     return(half*half*x)%MOD
+        
         def power(x,n):
             return pow(x,n,MOD)
         return (power(5, even_counts)*power(4,odd_counts))%MOD
