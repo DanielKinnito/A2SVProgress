c4791a0f1fcd5e46cbf01fbb47f466e0c72078ee
DanielKinnito <100097646+DanielKinnito@users.noreply.github.com>
2024-02-29 04:57:59 -0800

    Add solution for Binary Tree Zigzag Level Order Traversal

---
+++ camp 1 week 2/leetcode/binary-tree-zigzag-level-order-traversal.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import defaultdict
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        
+        dictionary = defaultdict(list)
+        def helper(root, index):
+            if not root:
+                return None
+            # print(dictionary)
+            dictionary[index].append(root.val)
+            helper(root.left, index + 1)
+            helper(root.right, index + 1)
+            
+            return root
+        
+        helper(root,0)
+
+        answer = []
+        for i in range(len(dictionary)):
+            answer.append(dictionary.get(i))
+        
+        for i in range(len(answer)):
+            if i % 2 != 0:
+                answer[i] = answer[i][::-1]
+        
+        return answer
