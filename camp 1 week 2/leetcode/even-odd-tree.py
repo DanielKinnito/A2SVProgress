2852a379864bef331dce7178b332785d713a6027
DanielKinnito <100097646+DanielKinnito@users.noreply.github.com>
2024-02-29 05:26:12 -0800

    Add solution for Even Odd Tree

---
+++ camp 1 week 2/leetcode/even-odd-tree.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    """
+        1. Just like zigzag binary question, go through the tree 
+        and put the elements in a dictionary at their depth
+        2. Then check if each set of lists satisfy the conditions
+    """
+
+    def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        
+        dictionary = defaultdict(list)
+    
+        def helper(root, index):
+                if not root:
+                    return None
+                dictionary[index].append(root.val)
+                helper(root.left, index + 1)
+                helper(root.right, index + 1)
+                
+                return root
+        
+        helper(root,0)
+
+        for i in range(len(dictionary)):
+            level = dictionary.get(i)
+            for j in range(len(level)):
+                if i % 2 == 0 and (level[j] % 2 == 0 or (j > 0 and level[j] <= level[j - 1])):
+                    return False
+                elif i % 2 == 1 and (level[j] % 2 != 0 or (j > 0 and level[j] >= level[j - 1])):
+                    return False
+
+        return True
