1a71d1bbb3993270b24972edbc54539c68b273b1
DanielKinnito <100097646+DanielKinnito@users.noreply.github.com>
2024-03-07 08:33:43 +0300

    Add solution for Design Circular Queue

--- camp 1 week 3/leetcode/design-circular-queue.py
+++ camp 1 week 3/leetcode/design-circular-queue.py
@@ -1,42 +1,46 @@
 class MyCircularQueue:
 
     def __init__(self, k: int):
-        self.queue = deque(maxlen=k)
-        self.size = k
+        self.queue = [0] * k
+        self.front = 0
+        self.rear = -1
+        self.size = 0
+        self.capacity = k
 
     def enQueue(self, value: int) -> bool:
-        if self.size > 0:
-            self.queue.append(value)
-            self.size -= 1
+        if self.size < self.capacity:
+            self.rear = (self.rear + 1) % self.capacity
+            self.size += 1
+            self.queue[self.rear] = value
             return True
         else:
             return False
 
     def deQueue(self) -> bool:
-        if self.queue:
-            self.queue.popleft()
-            self.size += 1
+        if self.size > 0:
+            self.front = (self.front + 1) % self.capacity
+            self.size -= 1
             return True
         else:
             return False
 
     def Front(self) -> int:
-        if self.queue:
-            return self.queue[0]
+        if self.size > 0:
+            return self.queue[self.front]
         else:
             return -1
 
     def Rear(self) -> int:
-        if self.queue:
-            return self.queue[-1]
+        if self.size > 0:
+            return self.queue[self.rear]
         else:
             return -1
 
     def isEmpty(self) -> bool:
-        return not self.queue
+        return self.size == 0
 
     def isFull(self) -> bool:
-        return self.size == 0
+        return self.capacity == self.size
 
 
 # Your MyCircularQueue object will be instantiated and called as such:
