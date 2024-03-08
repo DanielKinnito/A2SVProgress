6f61a9d3c386bfbdf0a430d1d6830e2f9c869b92
DanielKinnito <100097646+DanielKinnito@users.noreply.github.com>
2024-03-08 14:18:55 +0300

    Add solution for Interestingdrink

---
+++ camp 1 week 3/Codeforces/Interestingdrink.py
@@ -0,0 +1,13 @@
+import bisect
+
+n = int(input())
+
+nums = list(map(int, input().split()))
+nums.sort()
+
+k = int(input())
+
+for _ in range(k):
+    query = int(input())
+    
+    print(bisect.bisect_right(nums, query))
