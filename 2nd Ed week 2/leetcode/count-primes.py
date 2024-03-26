539b1dd3426df33b3d44f9d3c1c64e6d00eade44
DanielKinnito <100097646+DanielKinnito@users.noreply.github.com>
2024-03-26 23:20:16 +0300

    Add solution for Count Primes

---
+++ 2nd Ed week 2/leetcode/count-primes.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n == 0 or n == 1:
+            return 0
+        
+        primes = [True] * (n+1)
+        primes[0], primes[1] = False, False
+        
+        for i in range(2, int(n**0.5)+1):
+            if primes[i]:
+                for j in range(i*i, n, i):
+                    primes[j] = False
+                    
+        answer = [i for i in range(2, n) if primes[i]]
+        
+        return len(answer)
