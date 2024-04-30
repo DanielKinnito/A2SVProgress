b83fe49df042a0a8e5919ae273d09743f2d10183
DanielKinnito <100097646+DanielKinnito@users.noreply.github.com>
2024-04-30 18:51:01 +0300

    Add solution for Missing Number

---
+++ 2nd Ed week 7/leetcode/missing-number.py
@@ -0,0 +1,18 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        length = len(nums)
+        index = 0
+        while index < length:
+            correct_idx = nums[index]
+            if correct_idx == length:
+                index += 1
+            elif nums[index] != nums[correct_idx]:
+                nums[index], nums[correct_idx] = nums[correct_idx], nums[index]
+            else:
+                index += 1
+        
+        for i in range(length):
+            if i != nums[i]:
+                return i
+        
+        return length
