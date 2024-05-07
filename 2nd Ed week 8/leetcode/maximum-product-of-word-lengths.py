42e722be2527f35ab7b8fa13b2afa31d98697a03
DanielKinnito <100097646+DanielKinnito@users.noreply.github.com>
2024-05-07 18:13:16 +0300

    Add solution for Maximum Product of Word Lengths

--- 2nd Ed week 8/leetcode/maximum-product-of-word-lengths.py
+++ 2nd Ed week 8/leetcode/maximum-product-of-word-lengths.py
@@ -2,13 +2,20 @@ class Solution:
     def maxProduct(self, words: List[str]) -> int:
         answer = 0
         n = len(words)
+        
+        def bit_mask(word):
+            ret = 0
+            for c in word:
+                ret  = ret | 1 << (ord(c) - ord('a'))
+            return ret
+
+        compare = []
+        for word in words:
+            compare.append(bit_mask(word))
 
         for i in range(n):
-            first = set(words[i])
             for j in range(i + 1, n):
-                second = set(words[j])
-
-                if (first & second):
+                if (compare[i] & compare[j]) > 0:
                     continue
                 answer = max(answer, (len(words[i]) * len(words[j])))
         
