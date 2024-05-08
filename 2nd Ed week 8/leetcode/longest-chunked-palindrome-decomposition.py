3b96e665d56d9eb599421e045eefc73f6e386a90
DanielKinnito <100097646+DanielKinnito@users.noreply.github.com>
2024-05-08 18:54:13 +0300

    Add solution for Longest Chunked Palindrome Decomposition

---
+++ 2nd Ed week 8/leetcode/longest-chunked-palindrome-decomposition.py
@@ -0,0 +1,27 @@
+class Solution:
+    def longestDecomposition(self, text: str) -> int:
+        left, right = deque(), deque()
+        first, last = 0, len(text) - 1
+
+        count = 0
+
+        while first < last:
+            left.append(text[first])
+            right.appendleft(text[last])
+
+            if left == right:
+                count += 2
+                left = deque()
+                right = deque()
+
+            first += 1
+            last -= 1
+        
+        print(left)
+        print(right)
+        if first == last or (len(left) != 0 and len(right) != 0):
+            return count + 1
+        elif left != right:
+            return count + 1
+        else:
+            return count
