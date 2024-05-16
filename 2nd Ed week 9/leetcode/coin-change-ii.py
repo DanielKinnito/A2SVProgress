a430c9c118c497b14d8ad1f98626d99c6817eeb5
DanielKinnito <100097646+DanielKinnito@users.noreply.github.com>
2024-05-16 22:01:57 +0300

    Add solution for Coin Change II

--- 2nd Ed week 9/leetcode/coin-change-ii.py
+++ 2nd Ed week 9/leetcode/coin-change-ii.py
@@ -1,19 +1,15 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        def dp(idx, curr):
-            if idx >= n or curr < 0:
-                return 0
-            
-            if curr == 0:
-                return 1
-            
-            if (idx, curr) not in memo:
-                take = dp(idx, curr - coins[idx])
-                leave = dp(idx + 1, curr)
-                
-                memo[(idx, curr)] = take + leave
-            
-            return memo[(idx, curr)]
-        memo = {}
         n = len(coins)
-        return dp(0, amount)
+        dp = [[0] * (amount + 1) for _ in range(n + 1)]
+
+        for i in range(n + 1):
+            dp[i][0] = 1
+        
+        for i in range(1, n + 1):
+            for temp_am in range(1, amount + 1):
+                dp[i][temp_am] = dp[i - 1][temp_am]
+                if temp_am >= coins[i - 1]:
+                    dp[i][temp_am] += dp[i][temp_am - coins[i - 1]]
+
+        return dp[n][amount]
