698320b1e55adca9941f06b5efe8fea6aae8b98c
DanielKinnito <100097646+DanielKinnito@users.noreply.github.com>
2024-05-23 17:26:24 +0300

    Add solution for Dungeon Game

--- 2nd Ed week 10/leetcode/dungeon-game.py
+++ 2nd Ed week 10/leetcode/dungeon-game.py
@@ -2,20 +2,14 @@ class Solution:
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
         n = len(dungeon)
         m = len(dungeon[0])
-        memo = {}
-
-        def dp(i, j):
-            if i >= n or j >= m:
-                return float('inf')
-            if i == n - 1 and j == m - 1:
-                return max(1, 1 - dungeon[i][j])
-
-            if (i, j) not in memo:
-                right = dp(i, j + 1)
-                down = dp(i + 1, j)
-                min_health_needed = min(right, down) - dungeon[i][j]
-                memo[(i, j)] = max(1, min_health_needed)
-
-            return memo[(i, j)]
-
-        return dp(0, 0)
+        
+        dp = [[float('inf')] * (m + 1) for _ in range(n + 1)]
+        
+        dp[n][m-1] = dp[n-1][m] = 1
+        
+        for i in range(n - 1, -1, -1):
+            for j in range(m - 1, -1, -1):
+                min_health = min(dp[i+1][j], dp[i][j+1])
+                dp[i][j] = max(1, min_health - dungeon[i][j])
+        
+        return dp[0][0]
