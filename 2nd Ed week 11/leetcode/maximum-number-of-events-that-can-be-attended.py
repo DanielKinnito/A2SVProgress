84bc7cdb0b9b35388a0ddcf8737d2dd5c948a1d8
Daniel Mekonnen <100097646+DanielKinnito@users.noreply.github.com>
2024-06-11 19:49:56 +0300

    Add solution for Maximum Number of Events That Can Be Attended

--- 2nd Ed week 11/leetcode/maximum-number-of-events-that-can-be-attended.py
+++ 2nd Ed week 11/leetcode/maximum-number-of-events-that-can-be-attended.py
@@ -5,20 +5,20 @@ class Solution:
         
         events.sort(reverse=True)
         day = 0
-        max_events = 0
+        answer = 0
         heap = []
         
         while events or heap:
             while heap and heap[0] < day:
-                heapq.heappop(heap)
+                heappop(heap)
             
             while events and events[-1][0] <= day:
-                heapq.heappush(heap, events.pop()[1])
+                heappush(heap, events.pop()[1])
                 
             if heap:
-                heapq.heappop(heap)
-                max_events += 1
+                heappop(heap)
+                answer += 1
                 
             day += 1
         
-        return max_events
+        return answer
