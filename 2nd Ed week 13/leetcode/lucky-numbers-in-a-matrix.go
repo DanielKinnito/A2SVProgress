5832e61de7d4d6ed043ba190cefd899fe6609e1c
Daniel Mekonnen <100097646+DanielKinnito@users.noreply.github.com>
2024-07-26 07:35:10 -0700

    Add solution for Lucky Numbers in a Matrix

---
+++ 2nd Ed week 13/leetcode/lucky-numbers-in-a-matrix.go
@@ -0,0 +1,39 @@
+func luckyNumbers (matrix [][]int) []int {
+    n := len(matrix)
+    m := len(matrix[0])
+
+    minimums := make([]int, n)
+    maximums := make([]int, m)
+
+    for i:=0; i < n; i++ {
+        cur_min := matrix[i][0]
+        for j := 1; j < m; j++{
+            if cur_min >= matrix[i][j]{
+                cur_min = matrix[i][j]
+            }
+        }
+        minimums[i] = cur_min
+    }
+    
+    for j := 0; j < m; j++{
+        cur_max := matrix[0][j]
+        for i := 1; i < n; i++{
+            if cur_max < matrix[i][j]{
+                cur_max = matrix[i][j]
+            }
+        }
+        maximums[j] = cur_max
+    }
+    
+    var answer []int
+    
+    for i := 0; i < n; i++{
+        for j := 0; j < m; j++{
+            if matrix[i][j] == minimums[i] && matrix[i][j] == maximums[j]{
+                answer = append(answer, matrix[i][j])
+            }
+        }
+    }
+
+    return answer
+}
