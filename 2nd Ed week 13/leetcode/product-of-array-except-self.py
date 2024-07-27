ad90b43fda13e75de90d0ca77bfea59a2888d73e
Daniel Mekonnen <100097646+DanielKinnito@users.noreply.github.com>
2024-07-26 23:13:15 -0700

    Add solution for Product of Array Except Self

---
+++ 2nd Ed week 13/leetcode/product-of-array-except-self.py
@@ -0,0 +1,19 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        prefix = [1] * n
+        suffix = [1] * n
+        answer = [1] * n
+
+        prefix[0] = 1
+        for i in range(1, n):
+            prefix[i] = prefix[i - 1] * nums[i - 1]
+        
+        suffix[n - 1] = 1
+        for i in range(n - 2, -1, -1):
+            suffix[i] = suffix[i + 1] * nums[i + 1]
+        
+        for i in range(n):
+            answer[i] = prefix[i] * suffix[i]
+        
+        return answer
